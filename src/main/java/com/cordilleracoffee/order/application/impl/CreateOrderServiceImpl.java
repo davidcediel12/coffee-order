f251b1b7189411a4688729896d8bce6f3327b66a
David Cediel <esdago18@gmail.com>
2025-05-06 23:40:29 +0200

    Use a copy of order items to avoid lost data when saving the order

--- src/main/java/com/cordilleracoffee/order/application/impl/CreateOrderServiceImpl.java
+++ src/main/java/com/cordilleracoffee/order/application/impl/CreateOrderServiceImpl.java
@@ -4,6 +4,7 @@ package com.cordilleracoffee.order.application.impl;
 import com.cordilleracoffee.order.application.CreateOrderService;
 import com.cordilleracoffee.order.application.mapper.OrderMapper;
 import com.cordilleracoffee.order.domain.model.Order;
+import com.cordilleracoffee.order.domain.model.OrderItem;
 import com.cordilleracoffee.order.domain.repository.OrderItemRepository;
 import com.cordilleracoffee.order.domain.repository.OrderRepository;
 import com.cordilleracoffee.order.infrastructure.messaging.consumer.checkout.dto.CheckoutPerformed;
@@ -12,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -29,13 +32,15 @@ public class CreateOrderServiceImpl implements CreateOrderService {
 
         Order order = orderMapper.toDomain(checkoutPerformed);
 
+        List<OrderItem> orderItems = new ArrayList<>(order.getOrderItems());
+
         order = orderRepository.save(order);
 
-        for (var orderItem : order.getOrderItems()) {
+        for (var orderItem : orderItems) {
             orderItem.setId(UUID.randomUUID());
             orderItem.setOrder(order);
         }
-        orderItemRepository.saveAll(order.getOrderItems());
+        orderItemRepository.saveAll(orderItems);
 
         log.info("Order {} was created successfully", order.getId());
     }
